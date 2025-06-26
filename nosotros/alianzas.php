5ff784dfdf027bcb558bc5fa20a2d6c815026a34
Ever Lopez <elmartinez@asefimex.com>
2025-06-26 10:13:47 -0600

    ajuste url amigable modulo alianzas

--- nosotros/alianzas.php
+++ nosotros/alianzas.php
@@ -17,7 +17,7 @@
                 <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
                     <div class="icon-box">
                         <div class="icon"><i class="bx bx-file"></i></div>
-                        <h4><a href="../alianzas/?al=<?php echo $alia_value["id"]; ?>"><?php echo $alia_value["titulo"]; ?></a></h4>
+                        <h4><a href="../alianzas/<?php echo $alia_value["id"]; ?>"><?php echo $alia_value["titulo"]; ?></a></h4>
                         <p><?php echo $alia_value["descripcion"]; ?></p>
                         <p>Fecha:<?php echo " " . $alia_value["fecha_inicio"]; ?></p>
                     </div>
